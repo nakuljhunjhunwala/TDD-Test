21686511025efcb19927c3b313fe90f24cd68b63
nakuljhunjhunwala <jhunjhunwalanakul@gmail.com>
2024-06-27 20:01:13 +0530

    Refactor stringCalculator function to handle multiple numbers

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ function stringCalculator(str){
         return parseInt(str)
     }
 
-    return 0
+    let numbers = str.split(/,|\n/)
+    let sum = 0
+    for (let i = 0; i < numbers.length; i++) {
+        sum += parseInt(numbers[i])
+    }
+    return sum
 }
 
 module.exports = { stringCalculator };
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -10,3 +10,19 @@ test('returns the number for a single number input', () => {
     expect(stringCalculator('1')).toBe(1);
     expect(stringCalculator('2')).toBe(2);
   });
+
+  test('returns the sum of two comma-delimited numbers', () => {
+    expect(stringCalculator('1,2')).toBe(3);
+    expect(stringCalculator('2,3')).toBe(5);
+  });
+
+  test('returns the sum of two newline-delimited numbers', () => {
+    expect(stringCalculator('1\n2')).toBe(3);
+    expect(stringCalculator('2\n3')).toBe(5);
+  });
+
+  test('returns the sum of three or more numbers', () => {
+    expect(stringCalculator('1,2,3')).toBe(6);
+    expect(stringCalculator('1\n2,3')).toBe(6);
+    expect(stringCalculator('1\n2\n3')).toBe(6);
+  });
